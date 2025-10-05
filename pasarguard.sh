16abeb84d476383977ef57a6c254ea4646b3b7fd
mahdi <mahdi200320032003@gmail.com>
2025-10-05 19:56:15 +0330

    fix: update method for fetching server IP in backup function

--- pasarguard.sh
+++ pasarguard.sh
@@ -227,7 +227,7 @@ send_backup_to_telegram() {
         return
     fi
 
-    local server_ip=$(curl -4 -s ifconfig.me || echo "Unknown IP")
+    local server_ip=$(curl -s https://api.ipify.org || ip route get 1 | awk '{print $7; exit}')
     local latest_backup=$(ls -t "$APP_DIR/backup" | head -n 1)
     local backup_path="$APP_DIR/backup/$latest_backup"
 
